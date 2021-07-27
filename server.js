dffb0e7e691c4568986f10b3a4eccff2b16ed82b
saitergun <tiasweb@gmail.com>
2021-07-27 19:25:52 +0300

    q

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.get('/helo', async (req, res) => {
   res.json(['helö', uuid.v4()])
 })
 
-server.listen(process.env.SERVER_PORT || 3001, '0.0.0.0', () => {
-  console.log('Server is running...')
-})
+const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
